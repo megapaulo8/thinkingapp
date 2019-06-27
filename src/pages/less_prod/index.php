2f98d8c36a9b7f6fcfcb2fe5b12c44bb79dcbc8b
megapaulo8 <paulosantos194@hotmail.com>
2019-06-26 23:05:30 -0300

    fix : footer on view screens

--- src/pages/less_prod/index.php
+++ src/pages/less_prod/index.php
@@ -85,7 +85,7 @@
     <script>
         function isNumberKey(evt)
         {
-            var charCode = (evt.which) ? evt.which : event.keyCode
+            var charCode = (evt.which) ? evt.which : event.keyCode;
             if (charCode > 31 && (charCode < 48 || charCode > 57))
                 return false;
 
